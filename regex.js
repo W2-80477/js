d95b6e4aaef63476890ca573fd3688bc3a1f4882
Satyam Kumar <satyamkuamr.sk04@gamil.com>
2022-09-25 19:35:14 +0530

    code

--- regex.js
+++ regex.js
@@ -1,4 +1,16 @@
-const pattern= /\w\s\w/;
-const input ="Hello World";
-const res= pattern.test(input);
-console.log(res);
+// const pattern= /\w\s\w/;
+// const input ="Hello World";
+// const res= pattern.test(input);
+// console.log(res);
+
+// const pattern = /me/;
+// const input = "Find me"
+// const res = input.search(pattern);
+// console.log(res);
+
+
+// create email
+const pattern = /\S+@[a-z]+.\S+/;
+const input = "ab@/gmail.com"
+const res = pattern.test(input);
+// console.log(res);
