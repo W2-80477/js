dc1b778a49e6992f4928fa3802efe592e3482911
Satyam Kumar <satyamkumar.sk786@gmail.com>
2022-09-27 11:23:46 +0530

    stack changes

---
+++ stack.js
@@ -0,0 +1,32 @@
+class stack{
+    constructor(){
+        this.items=[];
+    }
+    push(element){
+        this.items.push(element);
+    }
+    pop(){
+        return this.items()
+    }
+    peek(){
+        return this.items[this.items.length-1]
+    }
+    isEmpty(){
+        return this.items.length==0
+    }
+    size(){
+        return this.items.length
+    }
+    print(){
+        console.log(this.items.toString());
+    }
+}
+// const stack =new stack();
+// console.log(stack.isEmpty());
+
+
+stack.push(20)
+stack.push(10)
+stack.push(30)
+console.log(stack.size());
+console.print();
